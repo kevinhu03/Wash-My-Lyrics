56627dbec56d78319262465a64bf5c03804d0b24
kevinhu03 <72530444+kevinhu03@users.noreply.github.com>
2020-10-14 17:56:10 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,74 @@
+$("button").click(function() {
+    let first = $(".first").val();
+    $(".firstlyric").text(first);    
+});
+
+$("button").click(function() {
+    let second = $(".second").val();
+    $(".secondlyric").text(second);    
+});
+
+$("button").click(function() {
+    let third = $(".third").val();
+    $(".thirdlyric").text(third);    
+});
+
+$("button").click(function() {
+    let fourth = $(".fourth").val();
+    $(".fourthlyric").text(fourth);    
+});
+
+$("button").click(function() {
+    let fifth = $(".fifth").val();
+    $(".fifthlyric").text(fifth);    
+});
+
+$("button").click(function() {
+    let sixth = $(".sixth").val();
+    $(".sixthlyric").text(sixth);    
+});
+
+$("button").click(function() {
+    let seventh = $(".seventh").val();
+    $(".seventhlyric").text(seventh);    
+});
+
+$("button").click(function() {
+    let eigth = $(".eigth").val();
+    $(".eigthlyric").text(eigth);    
+});
+
+$("button").click(function() {
+    let ninth = $(".ninth").val();
+    $(".ninthlyric").text(ninth);    
+});
+
+$("button").click(function() {
+    let tenth = $(".tenth").val();
+    $(".tenthlyric").text(tenth);    
+});
+
+$("button").click(function() {
+    let eleventh = $(".eleventh").val();
+    $(".eleventhlyric").text(eleventh);    
+});
+
+$("button").click(function() {
+    let eleventh = $(".eleventh").val();
+    $(".eleventhlyric").text(eleventh);    
+});
+
+$("button").click(function() {
+    let twelfth = $(".twelfth").val();
+    $(".twelfthlyric").text(twelfth);    
+});
+
+$("button").click(function() {
+    let thirteenth = $(".thirteenth").val();
+    $(".thirteenthlyric").text(thirteenth);    
+});
+
+$("button").click(function() {
+    let test = $(".first").val();
+    alert(test);
+});
